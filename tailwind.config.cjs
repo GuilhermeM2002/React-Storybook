3ad96aafa4a5c86b16de0b756872b77fdc21676a
Guilherme Ribeiro <108144644+GuilhermeM2002@users.noreply.github.com>
2022-10-16 11:04:28 -0300

    ci: Add publish storybook workflow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
-    fontSize:{
+    fontSize: {
       xs: 14,
       sm: 16,
       md: 18,
@@ -12,27 +12,30 @@ module.exports = {
       xl: 24,
       '2xl': 32,
     },
+
     colors: {
+      transparent: 'transparent',
+
       black: '#000',
       white: '#FFF',
 
-      gray:{
+      gray: {
         900: '#121214',
         800: '#202024',
-        400: '#7C7C8A',
-        200: '#C4C4CC',
-        100: '#E1E1E6',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
       },
 
-      cyan:{
-        500: '#81D8EF',
-        300: '#ABEDFF',
-      }
+      cyan: {
+        500: '#81d8f7',
+        300: '#9BE1FB'
+      },
     },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
-      }
+      },
     },
   },
   plugins: [],
